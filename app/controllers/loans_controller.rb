fb868c72dc37e7102f40ec8141950983e21b4553
Victoire De Gonneville <v.dechampvallier@gmail.com>
2020-11-18 14:58:52 +0100

    loan, edit, show, indexV5

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,2 +1,41 @@
 class LoansController < ApplicationController
+  before_action :set_loan, only: [:show, :destroy]
+
+
+  def show
+  end
+
+
+  def new
+    @jewellery = Jewellery.find(params[:jewellery_id])
+    @loan = Loan.new
+    @loans = Loan.all
+  end
+
+  def create
+    @loan = Loan.new(loan_params)
+    @jewellery = Jewellery.find(params[:jewellery_id])
+    @loan.jewellery = @jewellery
+    @loan.user = current_user
+    if @loan.save
+      redirect_to loan_path(@loan)
+    end
+  end
+
+  def destroy
+    @jewellery = @loan.jewellery
+    @loan.destroy
+    redirect_to jewellery_path(@jewellery)
+  end
+
+  private
+
+  def set_loan
+    @loan = Loan.find(params[:id])
+  end
+
+  def loan_params
+    params.require(:loan).permit(:from, :to)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :jewelleries, only: [:index, :show, :new, :create, :destroy] do
+  resources :jewelleries, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :loans, only: [:new, :create]
   end
-  resources :loans, only: [:destroy]
+  resources :loans, only: [:show, :destroy]
 end
