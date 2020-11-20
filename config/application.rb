ed2438720dbdf098ff5f7eaf9ca353cb923dc860
Victoire De Gonneville <v.dechampvallier@gmail.com>
2020-11-20 12:42:20 +0100

    javascript styling

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module AirJewellery
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.autoload_paths << "#{config.root}/app/validators"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
